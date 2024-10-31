991667f0045364d31178cb4a9f5e49d798cc21cd
bhupi <bhupendrapaliwal1983@gmail.com>
2024-10-31 15:28:15 +0530

    d7

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.css'
+import { Studentform } from './Studentform'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-center p-5" >WELCOME TO Student information</h1>
-        
+      <h1 className="text-center p-5 bg-success text-white" >WELCOME TO Student information</h1>
+  <Studentform/>
     </>
   )
 }
